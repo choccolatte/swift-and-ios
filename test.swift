bb4fe8c8af36398e7baf216c4f05470c5882eab9
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-04-20 01:18:25 +0530

    new changes in test file - 2004260118

--- test.swift
+++ test.swift
@@ -1,1 +1,11 @@
-print("hello world!")
+let greet = "hello"
+var name = "swift"
+print("\(greet), \(name)")
+
+// a func
+
+func addi(_ a: Int, _ b: Int){
+	return a * b
+}
+
+print(addi(5, 20)) // 100
